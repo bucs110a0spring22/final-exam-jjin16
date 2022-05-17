9a7150c9f8e61f656fae5cdf1e7579215c1e952b
Jiarong Jin <98533787+jjin16@users.noreply.github.com>
2022-05-17 03:32:10 +0000

    added while loop

--- main.py
+++ main.py
@@ -7,21 +7,40 @@ def main():
       print("here is a cute picture of a doggo!")
       dog=doggo.Doggo()
       print("click on the link:", dog)
-      one_more= input("would you like to see another doggo? y/n:")
-      if one_more == "y" or one_more == "Y":
-        dog=doggo.Doggo()
-        print("click on the link:", dog)
-      elif one_more == "n" or one_more == "N":
-        break
+      while True:
+        one_more= input("would you like to see another doggo? y/n:")
+        if one_more == "y" or one_more == "Y":
+          dog=doggo.Doggo()
+          print("click on the link:", dog)
+        elif one_more == "n" or one_more == "N":
+          print("okay, have a nice day!")
+          quit()
+          
+        
     elif team == "duck" or team == "Duck":
       print("here is a cute picture of a duck!")
-      #cat_fact=catfacts.Catfacts()
       duck=duckky.Duckky()
       print("click on the link:", duck)
+      while True:
+        one_more= input("would you like to see another ducky? y/n:")
+        if one_more == "y" or one_more == "Y":
+          duck=duckky.Duckky()
+          print("click on the link:", duck)
+        elif one_more == "n" or one_more == "N":
+          print("okay, have a nice day!")
+          quit()
     elif team == "fox" or team == "Fox":
       print("here is a cute fox picture!")
       fox= foxy.Foxy()
       print("Click on the link:", fox)
+      while True:
+        one_more= input("would you like to see another foxy? y/n:")
+        if one_more == "y" or one_more == "Y":
+          fox= foxy.Foxy()
+          print("click on the link:", fox)
+        elif one_more == "n" or one_more == "N":
+          print("okay, have a nice day!")
+          quit()
     else: 
       print("Please enter either 'dog', 'duck', or 'fox' Please!")
 main()
